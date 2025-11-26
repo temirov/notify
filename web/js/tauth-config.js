c4faac287f0ae1f29f62f3c73ed97a2dceb94f97
Vadym Tyemirov <temirov@gmail.com>
2025-11-25 23:47:38 -0800

    fix: allow ghttp runtime-config when API runs elsewhere

--- web/js/tauth-config.js
+++ web/js/tauth-config.js
@@ -4,3 +4,15 @@ window.PINGUIN_TAUTH_CONFIG =
     baseUrl: 'http://localhost:8081',
     googleClientId: '991677581607-r0dj8q6irjagipali0jpca7nfp8sfj9r.apps.googleusercontent.com',
   });
+
+if (!window.__PINGUIN_CONFIG__) {
+  window.__PINGUIN_CONFIG__ = {};
+}
+
+if (!window.__PINGUIN_CONFIG__.runtimeConfigUrl) {
+  window.__PINGUIN_CONFIG__.runtimeConfigUrl = 'http://localhost:8080/runtime-config';
+}
+
+if (!window.__PINGUIN_CONFIG__.apiBaseUrl) {
+  window.__PINGUIN_CONFIG__.apiBaseUrl = 'http://localhost:8080/api';
+}
